e7bf06e74d30f93f89b8be0977c361b710f76410
lirongf <619213582@qq.com>
2020-05-30 03:06:11 +0800

    All Start Create floder UnitySRPLearn

---
+++ UnitySRPLearn/Script/MyRenderPipelineAsset.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+
+[CreateAssetMenu(menuName = "Rendering/My PipeRenderLine")]/*此特性使得AssetCreate的列表中新增这个Asset功能*/
+public class MyRenderPipelineAsset : RenderPipelineAsset
+{
+    //返回render Object，我们写好的渲染管线在这里传送给unity
+    protected override IRenderPipeline InternalCreatePipeline()
+    {
+        return new Mypipeline();
+    }
+}
---
+++ UnitySRPLearn/Script/Mypipeline.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+//此类正式写为渲染管线流程
+public class Mypipeline : RenderPipeline
+{
+
+    /**
+     * Unity会根据激活的Camera调用Render管线
+     */
+    public override void Render(ScriptableRenderContext renderContext, Camera[] cameras)
+    {
+        base.Render(renderContext, cameras);
+        renderContext.DrawSkybox(cameras[0]);
+        renderContext.Submit();
+    }
+}
